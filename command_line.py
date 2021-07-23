1ab5b2832f1db97303c7c120cc1377d33ccfe9c0
Riccardo Verzeni <riccardo@linx.net>
2021-07-23 17:17:39 +0100

    Create command line script

--- command_line.py
+++ command_line.py
@@ -23,17 +23,16 @@ def parse_and_get_args():
 
   parser.add_argument(
     'config_file_path',
-    help='Config file with user details like username and password'
+    help='Config file with user details like username and password',
   )
 
   parser.add_argument(
     'hostname',
-    help='Hostname of the switch'
+    help='Hostname of the switch',
   )
 
   parser.add_argument(
-    'actions',
-    nargs='+',
+    'action',
     choices=[
       'diff',
       'replace',
@@ -41,24 +40,40 @@ def parse_and_get_args():
       'running'
     ],
     help=textwrap.dedent("""
-      Please choose one or multiple actions from below
+      Please choose one action from below
       'diff' Compare Running config with Candidate config.
       'replace' Replace Running config with Candidate config.
       'merge' Merge Candidate config with Running config.
       'running' Get running config from switch and save it to a file.
-      """)
+      
+      """),
   )
 
   parser.add_argument(
     '-s',
     '--save-config-file-path',
     dest='save_config_file_path',
+    default="running.conf",
     help=textwrap.dedent("""
       File path to save running config in.
       If no path is given than file will be saved in current dir
       with hostname-action.xml. For example for running config with
       hostname foo.bar foo.bar-running.xml will be created.
+      
       """)
+  )
+
+  parser.add_argument(
+    '-f',
+    '--format',
+    dest='format',
+    choices=["xml", "cli"],
+    default="xml",
+    help=textwrap.dedent("""
+    The format in which interact with the Nokia device.
+    If using a payload its format will overwrite this option.
+    
+    """)
   )
 
   parser.add_argument(
@@ -78,57 +93,63 @@ def parse_and_get_args():
       The default value is 0, the minimal log besides stack backtrace is given;
       Verbose level 1 enables debug level logging for pyocnos;
       Verbose level 2 emits ncclient debug level logging as well.
+      
       """)
   )
 
   args = parser.parse_args()
-  if any(action in args.actions for action in
+  if any(action in args.action for action in
          ['diff', 'replace', 'merge']) and not args.candidate_file_path:
     parser.error(
-      "diff, replace and merge actions requires -c, --candidate-file-path.")
+      "diff, replace and merge action requires -c, --candidate-file-path.")
   return args
 
 
-if len(sys.argv) < 4:
-  print("Incorrect args")
-  sys.exit(1)
+def main():
+  args = parse_and_get_args()
+  config_path = args.config_file_path
+  switch_name = args.hostname
+  operation = args.action
+  running_conf_path = args.save_config_file_path
+  candidate_conf_path = args.candidate_file_path
+  format = args.format
+
+  with open(config_path) as f:
+    config = yaml.safe_load(f)['config']
 
 
-config_path = sys.argv[1]
-switch_name = sys.argv[2]
-operation = sys.argv[3]
+  driver = napalm.get_network_driver('sros')
+  t1 = time.time()
+  with driver(hostname=switch_name,
+              username=config['username'],
+              timeout=180,
+              password=config['password']) as device:
+    if operation == 'running':
+      result = device.get_config(retrieve='running',
+                                 optional_args={"format": format}
+                                 )
+      with open(running_conf_path, 'w') as f:
+        f.write(result['running'])
+
+    elif operation == 'merge':
+      config = candidate_conf_path
+      device.load_merge_candidate(filename=config)
+      device.commit_config()
+
+    elif operation == 'replace':
+      config = candidate_conf_path
+      device.load_replace_candidate(filename=config)
+      device.commit_config()
+
+    elif operation == 'diff':
+      config = candidate_conf_path
+      device.load_replace_candidate(filename=config)
+      print(device.compare_config(optional_args={"json_format": True}))
+
+  t2 = time.time()
+
+  print(t2-t1)
 
 
-with open(config_path) as f:
-  config = yaml.safe_load(f)['config']
-
-
-driver = napalm.get_network_driver('sros')
-t1 = time.time()
-with driver(hostname=switch_name,
-            username=config['username'],
-            timeout=180,
-            password=config['password']) as device:
-  if operation == 'running':
-    result = device.get_config(retrieve='running', optional_args={"format": "cli"})
-    with open('running.conf', 'w') as f:
-      f.write(result['running'])
-  elif operation == 'merge':
-    config = sys.argv[4]
-    device.load_merge_candidate(filename=config)
-    device.commit_config()
-  elif operation == 'replace':
-    config = sys.argv[4]
-    device.load_replace_candidate(filename=config)
-    device.commit_config()
-  elif operation == 'diff':
-    config = sys.argv[4]
-    device.load_replace_candidate(filename=config)
-    print(device.compare_config(optional_args={"json_format": True}))
-  elif operation == 'merge-diff':
-    config = sys.argv[4]
-    device.load_merge_candidate(filename=config)
-    print(device.compare_config())
-t2 = time.time()
-
-print(t2-t1)
+if __name__ == '__main__':
+  main()
